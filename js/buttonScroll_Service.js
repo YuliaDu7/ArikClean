445787059e2af276bbb5e761a88a326c481c6e78
YuliaDu7 <156234993+YuliaDu7@users.noreply.github.com>
2024-05-21 19:35:53 +0300

    upload

---
+++ js/buttonScroll_Service.js
@@ -0,0 +1,49 @@
+﻿            $(document).ready(function () {
+                $(".scroll-me-1").click(function () {
+                    var x = $(window).scrollTop();
+                    $('html, body').animate({ scrollTop: $("*:contains('ניקיון ספות'):last").offset().top })
+                });
+               
+                    $(".scroll-me-2").click(function () {
+                        var x = $(window).scrollTop();
+                       $('html, body').animate({ scrollTop: $("*:contains('ניקוי ריפודי רכב'):last").offset().top })
+                    });
+
+               
+                 $(".scroll-me-3").click(function () {
+                        var x = $(window).scrollTop();
+                     $('html, body').animate({ scrollTop: $("*:contains('ניקוי מזרנים'):last").offset().top })
+                 });
+
+                  
+                $(".scroll-me-4").click(function () {
+                    var x = $(window).scrollTop();
+                    $('html, body').animate({ scrollTop: $("*:contains('ניקוי שטיחים'):last").offset().top })                });
+                       
+                            $(".scroll-me-5").click(function () {
+                                var x = $(window).scrollTop();
+                                $('html, body').animate({ scrollTop: $("*:contains('ניקוי כיסאות'):last").offset().top })                            });
+
+
+                            //$(".scroll-me-1").click(function () {
+                            //    var x = $(window).scrollTop();
+                            //    $(window).scrollTop( 425);
+                            //});
+                            //$(".scroll-me-2").click(function () {
+                            //    var x = $(window).scrollTop();
+                            //    $(window).scrollTop( 1300);
+                            //});
+                            //$(".scroll-me-3").click(function () {
+                            //    var x = $(window).scrollTop();
+                            //    $(window).scrollTop( 2500);
+                            //});
+                            //$(".scroll-me-4").click(function () {
+                            //    var x = $(window).scrollTop();
+                            //    $(window).scrollTop(4000);
+                            //});
+                            //$(".scroll-me-5").click(function () {
+                            //    var x = $(window).scrollTop();
+                            //    $(window).scrollTop(4600);
+                            //});
+
+                        });
